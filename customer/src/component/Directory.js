5ee4ce87da6c96b970c00906125e134a4425a00b
Poojan Sakhreliya <POOJANSAKHRELIYA12@GMAIL.COM>
2024-03-28 22:36:34 +0530

    add cateroryCard

--- customer/src/component/Directory.js
+++ customer/src/component/Directory.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import CategoryCard from './CategoryCard'
 
 const Directory = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_URL}/api/v1/category/fetchAllCategory`);
+        console.log(response);
+        setCategories(response.data); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData(); // call the function to fetch data when the component mounts
+  }, []);
   return (
+
  <div className='w-screen h-screen'>
        <div className='w-full h-full relative' >
         <img src={require('../images/3.jpg')} className='w-full h-full absolute'/> 
@@ -10,11 +27,9 @@ const Directory = () => {
 
      <div className='w-full flex justify-center items-center'>
        <div className='flex min-h-screen w-4/5 items-center justify-around flex-wrap'>
-          <CategoryCard/>
-          <CategoryCard/>
-          <CategoryCard/>
-          <CategoryCard/>
-          <CategoryCard/>
+          {
+            categories.map(category=> <CategoryCard category={category} />)
+          }
        </div>
      </div>
  </div>

b28003a06462ce072fd53a1d9d3912ea496dc457
Poojan Sakhreliya <POOJANSAKHRELIYA12@GMAIL.COM>
2024-06-08 22:13:59 +0530

    solve bug on send mail

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -2,24 +2,26 @@ const nodemailer = require('nodemailer');
 
 const sendEmail=async(email,otp)=>{
 
-    const transporter = nodemailer.createTransport({
-        host: "smtp.elasticemail.com",//elastic servername
-        port: 2525,//elastic port
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: 'poojansakhreliya007@gmail.com',  //elastic username
-            pass: '79D03683F5F0E42FE26FE5ED8D6F634F4F2C'//elastic password
-        }
-
-    });
-
     // const transporter = nodemailer.createTransport({
-    //     host: "Gmail",
+    //     host: "smtp.elasticemail.com",//elastic servername
+    //     port: 2525,//elastic port
+    //     secure: false, // true for 465, false for other ports
     //     auth: {
-    //         user: 'poojansakhreliya12@gmail.com',  
-    //         pass: "rhbppqqvkhgviedw"
+    //         user: 'poojansakhreliya007@gmail.com',  //elastic username
+    //         pass: '79D03683F5F0E42FE26FE5ED8D6F634F4F2C'//elastic password
     //     }
+
     // });
+
+    const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        host: "smtp.gmail.com",
+        port: 587,
+        auth: {
+            user: 'poojansakhreliya007@gmail.com',  
+            pass: process.env.EMAIL_APP_PASSWORD
+        }
+    });
     // console.log(email+"--->"+otp);
 
 
@@ -29,7 +31,7 @@ const sendEmail=async(email,otp)=>{
         subject: 'Sending Email for authentication',
         text: `For authetication Otp is : ${otp}`
     })
-    console.log(info+"from email function");
+    // console.log(info+"from email function");
  
 
 }

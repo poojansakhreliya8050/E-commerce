673b02ec25987f7288c3aa3a4cb00d2b28c9ef00
Poojan Sakhreliya <POOJANSAKHRELIYA12@GMAIL.COM>
2024-06-28 22:50:16 +0530

    add rating design on product card

--- customer/src/component/ProductCard.js
+++ customer/src/component/ProductCard.js
@@ -2,9 +2,10 @@ import React from 'react'
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { cartData } from '../redux/cart/cartSlice';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useGetCartQuery } from '../redux/cart/cartApiSlice';
 import { useAddToCartMutation, useRemoveItemFromCartMutation } from '../redux/cart/cartApiSlice'
+import RatingStar from './RatingStar';
 
 const Productcard = ({ product }) => {
   // console.log(product);
@@ -23,11 +24,11 @@ const Productcard = ({ product }) => {
   }
 
 
-  const removeItemFromCart=async()=>{
+  const removeItemFromCart = async () => {
     console.log("removeItemFromCart");
     try {
       if (user != null) {
-       await removeItemFromCartMutation({ userId: user._id, productId: product._id }).unwrap()
+        await removeItemFromCartMutation({ userId: user._id, productId: product._id }).unwrap()
       }
     } catch (err) {
       console.log(err);
@@ -50,8 +51,8 @@ const Productcard = ({ product }) => {
 
 
   return (
-    <div class="group w-[340px] overflow-hidden cursor-pointer flex flex-col group m-4   rounded-lg shadow-lg duration-300 hover:scale-105">
-      <div class="h-48 md:h-56 lg:h-[16rem]  border-2 border-white flex items-center justify-center text-white text-base mb-3 md:mb-5 overflow-hidden relative">
+    <div class="group w-[340px] overflow-hidden  flex flex-col group m-4   rounded-lg shadow-lg duration-300 hover:scale-105">
+      <Link to={"/productDetails"} class="h-48 md:h-56 lg:h-[16rem]  border-2 border-white flex items-center justify-center text-white text-base mb-3 md:mb-5 overflow-hidden relative">
         <img
           src={product.img}
           class=" object-cover w-full h-full scale-100 group-hover:scale-110 transition-all duration-400"
@@ -59,26 +60,31 @@ const Productcard = ({ product }) => {
         />
 
         <div class="absolute z-10 border-4 border-primary w-[95%] h-[95%] invisible group-hover:visible opacity-0 group-hover:opacity-100 group-hover:scale-90 transition-all duration-500"></div>
-      </div>
+      </Link>
+
       <p class=" block text-black text-center hover:text-primary transition-colors duration-150 text-lg md:text-xl mb-1">
         {product.productName}
       </p>
 
-      <p class="mb-4 font-light  text-sm md:text-sm text-center text-gray-950">
+      <p class="mb-1 font-light  text-sm md:text-sm text-center text-gray-950">
         {product.productDescription.length > 30 ? product.productDescription.substring(0, 30) + " ..." : product.productDescription}
       </p>
 
-      <div className="mb-4 text-sm md:text-sm text-center text-gray-950 font-light">
-      &#8377; {product.price}
+      <div className="mb-1 text-sm md:text-sm text-center text-gray-950 font-light">
+        &#8377; {product.price}
       </div>
-
+      <div className="mb-4 flex justify-center items-center">
+        <div>
+        <RatingStar rating={product.rating} />
+        </div>
+      </div>
       <div class="flex justify-center gap-x-3">
         {
           isAvailable == false || isAvailable == undefined ?
-            <button onClick={()=>addToCart()} class="group-hover:bottom-2 relative -bottom-12 rounded-md px-5 py-2 border border-primary text-primary hover:bg-primary  transition-all outline-none bg-black border-black text-white hover:text-black hover:bg-white font-bold">
+            <button onClick={() => addToCart()} class="group-hover:bottom-2 relative -bottom-12 rounded-md px-5 py-2 border border-primary text-primary hover:bg-primary  transition-all outline-none bg-black border-black text-white hover:text-black hover:bg-white font-bold">
               Add To Cart
             </button> :
-            <button onClick={()=>removeItemFromCart()} class="group-hover:bottom-2 relative -bottom-12 rounded-md px-5 py-2 border border-primary text-primary hover:bg-primary  transition-all outline-none bg-black border-black text-white hover:text-black hover:bg-white font-bold">
+            <button onClick={() => removeItemFromCart()} class="group-hover:bottom-2 relative -bottom-12 rounded-md px-5 py-2 border border-primary text-primary hover:bg-primary  transition-all outline-none bg-black border-black text-white hover:text-black hover:bg-white font-bold">
               Remove From Cart
             </button>
         }
